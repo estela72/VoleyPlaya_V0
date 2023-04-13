bad6065a72a0945943f5d18e1718c918aed29df1
p550968@arcelormittal.es <p550968@arcelormittal.es>
2023-04-13 15:40:16 +0200

    Ficheros que faltaban

---
+++ Federacion/VoleyPlaya.GestionWeb/Areas/Identity/Pages/Account/RegisterRequests.cshtml.cs
@@ -0,0 +1,56 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.ComponentModel.DataAnnotations;
+
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VoleyPlaya.GestionWeb.Areas.Identity.Pages.Account
+{
+    public class RegisterRequestsModel : PageModel
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public RegisterRequestsModel(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [BindProperty]
+        public List<IdentityUser> Requests { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            // Obtén la lista de usuarios pendientes de confirmación
+            Requests = _userManager.Users.Where(u => !u.EmailConfirmed).ToList();
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string? userId)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var user = await _userManager.FindByIdAsync(userId);
+
+            if (user == null)
+            {
+                // Maneja el caso en el que el usuario no sea encontrado
+                return NotFound();
+            }
+
+            // Confirma la cuenta del usuario
+            user.EmailConfirmed = true;
+            await _userManager.UpdateAsync(user);
+
+            // Obtén la lista de usuarios pendientes de confirmación
+            Requests = _userManager.Users.Where(u => !u.EmailConfirmed).ToList();
+            return Page();
+        }
+    }
+}
