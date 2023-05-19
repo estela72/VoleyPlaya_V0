fa184a0c905ff2360451404c296d730cfdd6df6e
p550968@arcelormittal.es <p550968@arcelormittal.es>
2023-05-19 23:09:51 +0200

    Fases finales generadas con cuadro de cruces

---
+++ Federacion/VoleyPlaya.Domain/AutomapperProfiles/PartidoCalendarioProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoleyPlaya.Domain.AutomapperProfiles
+{
+    public class PartidoCalendarioProfile : Profile
+    {
+        public PartidoCalendarioProfile()
+        {
+            CreateMap<VoleyPlaya.Repository.Models.TablaCalendario, VoleyPlaya.Domain.Models.PartidoCalendarioCircuito>()
+                .ReverseMap();
+        }
+    }
+}
---
+++ Federacion/VoleyPlaya.GestionWeb/Data/Migrations/20230518141515_jornadacircuito.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VoleyPlaya.GestionWeb.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class jornadacircuito : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Jornada",
+                table: "Tablas",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Jornada",
+                table: "Tablas");
+        }
+    }
+}
