9c529e58d5259381e55a9461e2cc9d6f772878f9
p550968@arcelormittal.es <p550968@arcelormittal.es>
2023-05-23 09:38:25 +0200

    Actas generadas. Primera prueba finalizada

---
+++ Federacion/VoleyPlaya.Domain/Utilities.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.RegularExpressions;
+
+namespace VoleyPlaya.Domain
+{
+    public static class Utilities
+    {
+        public static string ObtenerDigitosContinuos(string cadena)
+        {
+            string patron = @"\d+"; // Expresión regular para buscar dígitos continuos
+            MatchCollection coincidencias = Regex.Matches(cadena, patron);
+
+            string digitos = "";
+            foreach (Match coincidencia in coincidencias)
+            {
+                digitos += coincidencia.Value;
+            }
+
+            return digitos;
+        }
+        public static string ObtenerLetras(string cadena)
+        {
+            string patron = "[a-zA-Z]"; // Expresión regular para buscar letras
+            MatchCollection coincidencias = Regex.Matches(cadena, patron);
+
+            string letras = "";
+            foreach (Match coincidencia in coincidencias)
+            {
+                letras += coincidencia.Value;
+            }
+
+            return letras;
+        }
+    }
+}
