6662de5d55d55ddafd12662179c95883eca75008
p550968@arcelormittal.es <p550968@arcelormittal.es>
2023-06-02 16:19:37 +0200

    Arreglar la clasificación cuando se retira un equipo

---
+++ Federacion/VoleyPlaya.Repository/Enums/EnumEstadoEdicion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoleyPlaya.Repository.Enums
+{
+    public enum EnumEstadoEdicion
+    {
+       Registrada,
+       EnJuego,
+       Finalizada
+    }
+}
