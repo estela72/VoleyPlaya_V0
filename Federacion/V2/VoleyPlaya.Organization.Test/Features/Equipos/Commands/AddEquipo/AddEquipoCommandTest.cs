c3562da811e4ab48aeca6fdf2f59c9e1b68e3b1e
p550968@arcelormittal.es <p550968@arcelormittal.es>
2023-08-02 17:56:59 +0200

    Application, repository y tests de equipos

---
+++ Federacion/V2/VoleyPlaya.Organization.Test/Features/Equipos/Commands/AddEquipo/AddEquipoCommandTest.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+
+using GenericLib;
+
+using MediatR;
+
+using Moq;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using VoleyPlaya.Organization.Application.DTOs;
+using VoleyPlaya.Organization.Application.Features.Equipos.Commands.AddEquipo;
+using VoleyPlaya.Organization.Application.Mappings;
+using VoleyPlaya.Organization.Infraestructure.Repositories;
+using VoleyPlaya.Organization.Test.Mocks;
+
+namespace VoleyPlaya.Organization.Test.Features.Equipos.Commands.AddEquipo
+{
+    public class AddEquipoCommandTest
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<UnitOfWork> _unitOfWork;
+        public AddEquipoCommandTest()
+        {
+            _unitOfWork = new MockUnitOfWork().GetUnitOfWork();
+            var mapperConfig = new MapperConfiguration(c =>
+            {
+                c.AddProfile<MappingProfile>();
+            });
+            _mapper = mapperConfig.CreateMapper();
+        }
+
+        [Fact]
+        public async Task AddEquipoNew()
+        {
+            var handler = new AddEquipoCommandHandler(_unitOfWork.Object, _mapper);
+            var request = new AddEquipoCommand();
+            request.Nombre = "Equipo-New";
+            var result = await handler.Handle(request, CancellationToken.None);
+            Assert.NotNull(result);
+            Assert.Equal("Equipo-New", result.Nombre);
+        }
+        [Fact]
+        public async Task AddEquipoExistent()
+        {
+            var existente = (await _unitOfWork.Object.EquipoRepository.GetAllAsync()).FirstOrDefault();
+
+            var handler = new AddEquipoCommandHandler(_unitOfWork.Object, _mapper);
+            var request = new AddEquipoCommand();
+            request.Nombre = existente.Nombre;
+            await Assert.ThrowsAsync<VoleyPlayaDomainException>(async () => await handler.Handle(request, CancellationToken.None));
+        }
+    }
+}
