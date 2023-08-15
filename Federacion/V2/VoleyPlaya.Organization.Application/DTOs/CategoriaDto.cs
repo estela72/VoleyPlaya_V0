4adde8838ad7c4f0846045c39359ecee3a8ca54c
p550968@arcelormittal.es <p550968@arcelormittal.es>
2023-08-15 16:55:44 +0200

    Reorganizar y crear una solución para únicamente el voley playa

--- Federacion/V2/VoleyPlaya.Organization.Application/DTOs/CategoriaDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace VoleyPlaya.Organization.Application.DTOs;
-
-public record CategoriaDto(int Id, string Nombre);
--- Federacion/V2/VoleyPlaya.Organization.Application/DTOs/TablaDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace VoleyPlaya.Organization.Application.DTOs;
-
-public record TablaDto(int Id, string Nombre, string Equipo1, string Equipo2, string Ronda);
